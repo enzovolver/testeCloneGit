bd30f32ecdc74e33313936ecd3ba033d0516576b
mikael1909 <kanto3210>
2020-02-14 08:19:47 -0300

    idade -1 :)

--- teste.c
+++ teste.c
@@ -3,6 +3,13 @@
 
 int main(){
 
+    int idade;
+    do{
+        printf("Sua idade: ");
+        scanf("%d", &idade);
+    }while(idade<0 || idade>100);
 
+    printf("\nSua idade menos 1 é: %d\n", --idade);
     return 0;
-}
+
+    }
